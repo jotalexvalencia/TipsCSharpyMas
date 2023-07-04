fc3a1dcba6828c46c2f01efbffd7bac33642ad89
Alex Valencia <jorgealexandervalencia@gmail.com>
2023-07-04 10:05:17 -0500

    Agregar archivos de proyecto.

---
+++ programa02_03/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace programa02_03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CPersona per1 = new CPersona();
+
+            CPersona per2 = new CPersona("Juan", 27);
+
+            per1.muestra();
+            per2.muestra();
+        }
+    }
+}
