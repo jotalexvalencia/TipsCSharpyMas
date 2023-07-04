039e2af203e8d536d7357836fac18bec47c4f0b4
Alex Valencia <jorgealexandervalencia@gmail.com>
2023-07-04 11:22:04 -0500

    programa_03_01 se usan los atributos estaticos

---
+++ programa03_01/CCompu.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace programa03_01
+{
+    class CCompu
+    {
+        private static double tipoCambio;
+
+        private string procesador = "";
+        private int memoria;
+        private double costo;
+
+        public CCompu()
+        {
+            procesador = "i3";
+            memoria = 2;
+            costo = 350;
+        }
+        public CCompu(string pProcesador, int pMemoria, double pCosto)
+        {
+            procesador = pProcesador;
+            memoria = pMemoria;
+            costo = pCosto;
+            
+        }
+        public double TipoCambio
+        {
+            set{ tipoCambio = value; }
+        }
+        public void MuestraDolares()
+        {
+            Console.WriteLine("Procesador: {0}, memoria: {1}",procesador,memoria);
+            Console.WriteLine("Costo $USD:{0}",costo);
+        }
+        public void MuestraPesos()
+        {
+            Console.WriteLine("Procesador: {0}, memoria: {1}", procesador, memoria);
+            Console.WriteLine("Costo $:{0}, TC:{1}", costo*tipoCambio,tipoCambio);
+        }
+    }
+}
---
+++ programa03_01/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace programa03_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CCompu compu1 = new CCompu();
+            CCompu compu2 = new CCompu("i5", 4, 450);
+            CCompu compu3 = new CCompu("i7", 8, 670);
+
+            compu1.TipoCambio = 15.70; // con esto asignamos tipo de cambio 15.70 a comu2 y compu3
+
+            // Mostramos
+            compu1.MuestraDolares();
+            compu1.MuestraPesos();
+            Console.WriteLine("-----");
+
+            compu2.MuestraDolares();
+            compu2.MuestraPesos();
+            Console.WriteLine("-----");
+
+            compu3.MuestraDolares();
+            compu3.MuestraPesos();
+            Console.WriteLine("-----");
+
+            Console.WriteLine("================================");
+
+            // Hacemos el cambio, debe reflejarse en todos
+
+            compu1.TipoCambio = 18.80;
+            // Mostramos
+            compu1.MuestraDolares();
+            compu1.MuestraPesos();
+            Console.WriteLine("-----");
+
+            compu2.MuestraDolares();
+            compu2.MuestraPesos();
+            Console.WriteLine("-----");
+
+            compu3.MuestraDolares();
+            compu3.MuestraPesos();
+            Console.WriteLine("-----");
+        }
+    }
+}
