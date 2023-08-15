211c8740907f68eb5229809a6f2e42502d5c11e3
Alex Valencia <jorgealexandervalencia@gmail.com>
2023-08-15 17:31:51 -0500

    constructor estatico se comparte es decir si se instancia n veces solo el constructor es invocado una sola vez la vez de la primera instanciacion

---
+++ Tip 8c ConstuctorEstatico/MiClase.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tip_8c_ConstuctorEstatico
+{
+    class MiClase
+    {
+        private int valor = 20;
+        private static int numero;
+
+        static MiClase()
+        {
+            numero = 33;
+            Console.WriteLine("Saludos desde el constructor");
+        }
+
+        public void ponValor(int pValor)
+        {
+            valor= pValor;
+        } 
+        public void ponNumero(int pNumero)
+        {
+            numero= pNumero;
+        } 
+        public void muestraDatos()
+        {
+            Console.WriteLine("Valor={0}, numero={1}",valor,numero);
+        }
+    }
+}
---
+++ Tip 8c ConstuctorEstatico/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tip_8c_ConstuctorEstatico
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Creamos dos objetos
+            MiClase obj1 = new MiClase();
+            MiClase obj2 = new MiClase();
+
+            Console.WriteLine("Sin cambio");
+            obj1.muestraDatos();
+            obj2.muestraDatos();
+
+            // cambiamos
+            obj1.ponValor(400);
+            obj2.ponNumero(555);
+            Console.WriteLine("Con cambios");
+            obj1.muestraDatos();
+            obj2.muestraDatos();
+
+            // creamos otra instancia
+            MiClase obj3 = new MiClase();
+            Console.WriteLine("Despues de la instancia");
+            obj1.muestraDatos();
+            obj2.muestraDatos();
+            obj3.muestraDatos();
+
+        }
+    }
+}
