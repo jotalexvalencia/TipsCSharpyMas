5fd937fc87c492eee48ed6849413d85c04ade216
Alex Valencia <jorgealexandervalencia@gmail.com>
2023-08-15 15:47:06 -0500

    Tip 8a dato estatico se comparte el dato

---
+++ Tip 8a DatoEstatico/MiClase.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tip_8a_DatoEstatico
+{
+    class MiClase
+    {
+        private int valor = 50;
+        private static int numero = 10;
+
+        public void ponValor(int pValor)
+        {
+            valor = pValor;
+        }
+
+        public void ponNumero(int pNumero)
+        {
+            numero = pNumero;
+        }
+
+        public void muestraDatos()
+        {
+            Console.WriteLine("Valor={0}, numero={1},",valor,numero);
+        }
+    }
+}
---
+++ Tip 8a DatoEstatico/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tip_8a_DatoEstatico
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Creamos tres instancias
+            MiClase obj1 = new MiClase();
+            MiClase obj2 = new MiClase();
+            MiClase obj3 = new MiClase();
+
+            Console.WriteLine("Imprimimos antesd e cambios");
+            obj1.muestraDatos();
+            obj2.muestraDatos();
+            obj3.muestraDatos();
+
+            // cambiamos datos e imprimimos
+            obj1.ponValor(3);
+            obj2.ponValor(57);
+            obj3.ponValor(140);
+            Console.WriteLine("Imprimimos con cambios a valor");
+            obj1.muestraDatos();
+            obj2.muestraDatos();
+            obj3.muestraDatos();
+
+            // Cambiamos el numero solo en un objeto
+            obj2.ponNumero(300);
+            Console.WriteLine("Imprimimos con cambio a número");
+            obj1.muestraDatos();
+            obj2.muestraDatos();
+            obj3.muestraDatos();
+
+        }
+    }
+}
