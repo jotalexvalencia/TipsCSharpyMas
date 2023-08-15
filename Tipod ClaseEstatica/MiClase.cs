75c747f447e4a1f79ab7374103b2fa98eb9400da
Alex Valencia <jorgealexandervalencia@gmail.com>
2023-08-15 17:45:55 -0500

    Tip8d clase estatica

---
+++ Tipod ClaseEstatica/MiClase.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Tipod_ClaseEstatica
+{
+    static class MiClase
+    {
+        private static int valor = 20;
+        private static int numero;
+        internal static object ponnumero;
+
+        static MiClase()
+        {
+            numero = 33;
+            Console.WriteLine("Saludos desde el constructor");
+        } 
+        public static void ponValor(int pValor)
+        {
+            valor = pValor;
+        }
+        public static void ponNumero(int pNumero)
+        {
+            numero = pNumero;
+        }
+        public static void muestraDatos()
+        {
+            Console.WriteLine("valor={0}, numero={1}",valor,numero);
+        }
+
+    }
+}
---
+++ Tipod ClaseEstatica/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tipod_ClaseEstatica
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //MiClase obj1 = new MiClase();
+
+            Console.WriteLine("Sin cambio");
+            MiClase.muestraDatos();
+
+            
+            MiClase.ponValor(400);
+            MiClase.ponNumero(555);
+            Console.WriteLine("Con cambios");
+            MiClase.muestraDatos();
+        }
+    }
+}
