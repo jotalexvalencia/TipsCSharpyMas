025d6c4da09c3b435ac9e038af0bd5195f18705f
Alex Valencia <jorgealexandervalencia@gmail.com>
2023-08-15 14:36:31 -0500

    Tip7 readonly y const

---
+++ Tip8 Entendiendo static de una vez por todas/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tip8_Entendiendo_static_de_una_vez_por_todas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
