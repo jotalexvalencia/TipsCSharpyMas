5b8d8ddba3d47482f2f320d600875a2767b05398
Alex Valencia <jorgealexandervalencia@gmail.com>
2023-08-15 15:13:36 -0500

    Tip 8: metodo estatico y variable estatica

---
+++ Tip8 Entendiendo static de una vez por todas/MiClase.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tip8_Entendiendo_static_de_una_vez_por_todas
+{
+    class MiClase
+    {
+        private int valor = 50;
+        private static int numero = 10;
+
+        public void saluda()
+        {
+            Console.WriteLine("Solo saludo si hay instancia de la clase");
+        }
+
+        public static void mensaje()
+        {
+            Console.WriteLine("Puedo dar el mensaje aun sin instancia");
+        }
+
+        // El metodo estatico no puede usar un dato normal
+        //public static void imprimeValor()
+        //{
+        //    Console.WriteLine("El valor es {0}", valor);
+        //}
+
+        public static void imprimeNumero()
+        {
+            Console.WriteLine("El numero es {0}", numero);
+        }
+    }
+}
--- Tip8 Entendiendo static de una vez por todas/Program.cs
+++ Tip8 Entendiendo static de una vez por todas/Program.cs
@@ -6,10 +6,21 @@ using System.Threading.Tasks;
 
 namespace Tip8_Entendiendo_static_de_una_vez_por_todas
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
+            // No puedo invocar un método sin instancia, marca error
+            //MiClase.saluda();
+
+            // De esta forma si funciona
+            MiClase miObjeto = new MiClase();
+            miObjeto.saluda();
+
+            // Un método estatico si puede ser invocado sin instancia
+            //MiClase.mensaje();
+            //MiClase.imprimeValor();
+            MiClase.imprimeNumero();
         }
     }
 }
