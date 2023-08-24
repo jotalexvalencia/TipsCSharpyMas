d99cdb601c18f23e8493fa432a2d47ea47d59b8b
Alex Valencia <jorgealexandervalencia@gmail.com>
2023-08-24 15:55:32 -0500

    Tip18: Override métodos clase object

---
+++ Tip18OverrideClaeObject/CPersona.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tip18OverrideClaeObject
+{
+    class CPersona:Object
+    {
+        private string nombre;
+        private int edad;
+
+        public CPersona(string pNombre, int pEdad)
+        {
+            nombre = pNombre;
+            edad = pEdad;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendFormat("Nombre: {0}, edad: {1}", nombre,edad);
+
+            return sb.ToString();
+        }
+
+        public override bool Equals(object obj)
+        {
+            if(obj !=null && obj is CPersona)
+            {
+                CPersona temp = (CPersona)obj;
+
+                if(nombre == temp.nombre && edad == temp.edad)
+                    return true;
+            }
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return nombre.GetHashCode()+edad.GetHashCode();
+        }
+
+    }
+}
---
+++ Tip18OverrideClaeObject/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tip18OverrideClaeObject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CPersona Juan = new CPersona("Juan",27);
+
+            // ToString que ya se hizo override
+            Console.WriteLine("--- ToString ---");
+            Console.WriteLine(Juan.ToString());
+
+            // Equals ya se hizo override
+            CPersona Maria = new CPersona("Maria", 20);
+            Console.WriteLine("--- Equals ---");
+            if (Juan.Equals(Maria))
+                Console.WriteLine("iguales");
+            else
+                Console.WriteLine("diferentes");
+
+            CPersona Juan2 = new CPersona("Juan", 27);
+            if (Juan.Equals(Juan2))
+                Console.WriteLine("iguales");
+            else
+                Console.WriteLine("diferentes");
+
+            // GetHashCode ya se hizo override
+            Console.WriteLine("--- GetHashCode ---");
+            Console.WriteLine(Juan.GetHashCode());
+        }
+    }
+}
