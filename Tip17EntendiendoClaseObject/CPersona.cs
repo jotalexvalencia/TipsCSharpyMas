4c1aaa57d355b7a10e29b206a5578fe85bd71230
Alex Valencia <jorgealexandervalencia@gmail.com>
2023-08-24 14:22:24 -0500

    Tip17 Entendiendo la clase Object

---
+++ Tip17EntendiendoClaseObject/CPersona.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tip17EntendiendoClaseObject
+{
+    class CPersona:Object
+    {
+        private string nombre;
+        private int edad;
+
+        public CPersona(string pNombre, int pEdad)
+        {
+            nombre = pNombre;
+            edad = pEdad;
+        }
+
+        
+    }
+}
---
+++ Tip17EntendiendoClaseObject/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tip17EntendiendoClaseObject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CPersona Juan = new CPersona("Juan", 27);
+
+            // Vemos el comportamiento de ToString
+            Console.WriteLine("--- ToString ---");
+            Console.WriteLine(Juan.ToString());
+
+            // Vemos el compotamiento de GetType
+            Console.WriteLine("--- GetType ---");
+            Console.WriteLine(Juan.GetType());
+            Console.WriteLine(Juan.GetType().BaseType);
+
+            CPersona Maria = new CPersona("Maria", 20);
+
+            // Vemos el comportamiento de equals
+            Console.WriteLine("--- Equals ---");
+            if (Juan.Equals(Maria))
+                Console.WriteLine("iguales");
+            else
+                Console.WriteLine("diferentes");
+
+            CPersona Juan2 = new CPersona("Juan", 27);
+
+            if(Juan.Equals(Juan2))
+                Console.WriteLine("iguales");
+            else
+                Console.WriteLine("diferentes");
+
+            if(object.Equals(Juan, Juan2))
+                Console.WriteLine("iguales");
+            else
+                Console.WriteLine("diferentes");
+
+            Object Maria2 = Maria;
+
+            if(Maria.Equals(Maria2))
+                Console.WriteLine("iguales");
+            else
+                Console.WriteLine("diferentes");
+
+            // Vemos como trabaja ReferenceEquals
+
+            Console.WriteLine("--- ReferenceEquals ---");
+
+            if(Object.ReferenceEquals(Maria,Maria2))
+                Console.WriteLine("iguales");
+            else
+                Console.WriteLine("diferentes");
+
+            if(!Object.ReferenceEquals(Juan,Juan2))
+                Console.WriteLine("iguales");
+            else
+                Console.WriteLine("diferentes");
+
+            // Vemos el comportamiento de GetHashCode
+            Console.WriteLine("--- GetHashCode ---");
+            Console.WriteLine(Juan.GetHashCode());
+        }
+    }
+}
