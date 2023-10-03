12f68c758c38de7f04f5f18b1412fc3047e8b65f
Alex Valencia <jorgealexandervalencia@gmail.com>
2023-10-03 15:06:07 -0500

    tip 24 full

---
+++ Tip24_PropiedadesAutomaticas/CPrueba.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tip24_PropiedadesAutomaticas
+{
+    class CPrueba
+    {
+        private int valor;
+
+        // Propiedad tradicional
+
+        public int Valor
+        {
+            get { return valor; }
+            set { valor = value; }
+        }
+
+        // Propiedad automatica
+
+        public int Costo { get; set; } = 5;
+
+        public CPrueba()
+        {
+            valor = 10;
+        }
+
+        public void MuestraDatos()
+        {
+            Console.WriteLine("Valor es = {0}, Costo es {1}",valor,Costo);
+        }
+    }
+}
---
+++ Tip24_PropiedadesAutomaticas/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tip24_PropiedadesAutomaticas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Creamos instancia
+            CPrueba objeto = new CPrueba();
+
+            objeto.MuestraDatos();
+
+            int n = objeto.Valor;
+            Console.WriteLine("El valor de n es {0}", n);
+
+            int m = objeto.Costo;
+            Console.WriteLine("El costo de m es {0}", m);
+        }
+    }
+}
