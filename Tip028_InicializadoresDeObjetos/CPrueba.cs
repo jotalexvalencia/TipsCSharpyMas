70d6c327d4f66209eb558d73bf9f56da320ab9a3
Alex Valencia <jorgealexandervalencia@gmail.com>
2023-10-04 11:10:42 -0500

    Tip028 Inicializadores de objetos: ojo rompe el encapsulamiento

---
+++ Tip028_InicializadoresDeObjetos/CPrueba.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tip028_InicializadoresDeObjetos
+{
+    class CPrueba
+    {
+        public int a;
+        public int b;
+        public string c;
+
+        public CPrueba()
+        {
+            
+        }
+
+        public CPrueba(string pC)
+        {
+            c = pC;
+        }
+
+        public override string ToString()
+        {
+            return String.Format("{0}, {1}, {2}", a, b, c);
+        }
+    }
+}
---
+++ Tip028_InicializadoresDeObjetos/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tip028_InicializadoresDeObjetos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Inicializadores de objeto
+            // Permite colcoar valores facilmente durante la construcción
+            // Solo se usa con variables publicas
+            // Pone en riesgo el encapsulamiento
+
+            // Inicilizamos
+            CPrueba p1 = new CPrueba { a = 1, b = 2, c = "Hola a todos" };
+
+            Console.WriteLine(p1);
+
+            CPrueba p2 = new CPrueba("Saludos") { a = 5, b = 10 };
+
+            Console.WriteLine(p2);
+        }
+    }
+}
