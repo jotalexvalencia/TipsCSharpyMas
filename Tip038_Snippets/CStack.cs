b7e2d8d01b28328b8d1e7ea6a905891c0114e245
Alex Valencia <jorgealexandervalencia@gmail.com>
2023-10-06 15:04:24 -0500

    Tip038: Snippets: todos con tab , ~ destructor, class, ctor constructor, cw Console.WriteLine();, do doWhiele, for, foreach,forr for regresivo, if, switch, while, prop propiedad automatica, exception, interface, mbox MessageBox

---
+++ Tip038_Snippets/CStack.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tip038_Snippets
+{
+    // Hacemos una implementacion basada en lista ligada
+    class CStack
+    {
+        
+    }
+}
