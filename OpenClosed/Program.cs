d6a1f3bf3a3875fe4ba54d94be6cfc850d99d697
Alex Valencia <jorgealexandervalencia@gmail.com>
2023-10-17 14:40:45 -0500

    OpenClosed principio sin aplicar ya que se tuvo que modificar la clase CTienda

---
+++ OpenClosed/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenClosed
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<CProducto> misProductos = new List<CProducto>
+            {
+                new CProducto("Papas",1,12.5),
+                new CProducto("Analgesico",2,23.40),
+                new CProducto("Tomate",1,30),
+                new CProducto("Manzana",1,25),
+                new CProducto("Antibiotico",2,89),
+                new CProducto("Antiacido",2,43)
+            };
+
+            CTienda miTienda = new CTienda(misProductos);
+            miTienda.calcularInventario();
+        }
+    }
+}
