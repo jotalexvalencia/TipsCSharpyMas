e2083ee2fab05bf13783d025d9fc29d386837c5e
Alex Valencia <jorgealexandervalencia@gmail.com>
2023-10-17 11:08:54 -0500

    Principio de Responsabilidad única 1 : no se aaplica el principio y la clase empleado debe cambiar no solo cuando cambia empleado sino cuando cambia impuesto, mal diseño.

---
+++ SingleResponsibility1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingleResponsibility1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CEmpleado empleado = new CEmpleado("Juan", "Programador", 23, 18000);
+            Console.WriteLine(empleado);
+            empleado.PagarImpuesto();
+        }
+    }
+}
