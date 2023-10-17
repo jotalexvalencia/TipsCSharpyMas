d7a193ad88fff9757d4c448f8946e13a1df09c22
Alex Valencia <jorgealexandervalencia@gmail.com>
2023-10-17 11:43:27 -0500

    SingleResponsabilityRefactor: se creo la clase CHacienda la cual se encargará de lo referente a impuestos, dejando a CEmpleado solamente lo referente a empleados

---
+++ SingleResponsabilityRefactor/CEmpleado.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingleResponsabilityRefactor
+{
+    // Hacemos refactor de CEmpleado para dejarla solamente una responsabilidad
+    class CEmpleado
+    {
+        private string nombre;
+        private string puesto;
+        private int edad;
+        private double sueldo;
+
+        public string Nombre { get => nombre; set => nombre = value; } 
+        public double Sueldo { get => sueldo; set => sueldo = value; }
+
+        public CEmpleado(string pNombre, string pPuesto, int pEdad, double pSueldo)
+        {
+            nombre = pNombre;
+            puesto = pPuesto;
+            edad = pEdad;
+            sueldo = pSueldo;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0},{1},{2},{3}", nombre, puesto, edad, sueldo);
+        }
+
+        // Aqui van otros metodos propios de empleado
+        // AsignarHorario
+        // ProyectoActual
+    }
+}
---
+++ SingleResponsabilityRefactor/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingleResponsabilityRefactor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CEmpleado empleado = new CEmpleado("Juan", "Programador", 23, 18000);
+
+            Console.WriteLine(empleado);
+            Console.WriteLine(CHacienda.CalcularImpuesto(empleado));
+            CHacienda.PagarImpuesto(empleado);
+        }
+    }
+}
