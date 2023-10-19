b0979bc55fdf61cc44f4d0e7bcf4466bfb126e0e
Alex Valencia <jorgealexandervalencia@gmail.com>
2023-10-19 10:43:33 -0500

    DependencyInversion: sin aplicar

---
+++ DependencyInversion/Almacen.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DependencyInversion
+{
+    class Almacen
+    {
+        private List<Producto> inventario;
+
+        // Nos vemos forzados a crear la propiedad para que se pueda contar el inventario
+        // Al ser una propiedad de tipo List forzamos a Auditor a trabajar con List
+        public List<Producto> Inventario { get => inventario; set => inventario = value; }
+
+        public Almacen()
+        {
+            inventario = new List<Producto>();
+        }
+
+        public void AdicionarProducto(Producto pProducto)
+        {
+            inventario.Add(pProducto);
+            Console.WriteLine("Adicionamos {0}", pProducto.Nombre);
+        }
+    }
+}
---
+++ DependencyInversion/Auditor.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DependencyInversion
+{
+    class Auditor
+    {
+        private Almacen miAlmacen;
+
+        // Estan fuertemente acoplados, solamente puede trabajar con Almacen
+        public Auditor(Almacen pAlmacen)
+        {
+            miAlmacen = pAlmacen;
+        }
+
+        public double totalesAlimentos()
+        {
+            double total = 0;
+
+            foreach (Producto p in miAlmacen.Inventario)
+            {
+                if(p.Tipo == 0)
+                {
+                    Console.WriteLine(p);
+                    total += p.Costo;
+                }
+            }
+
+            return total;
+        }
+    }
+}
